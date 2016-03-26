7d9cdd92f01328fe99e627b0c1779f53358c3d4d
XaviPortell <xavi.portell@hotmail.com>
2016-03-26 18:22:34 +0100

    Redundant code
    Delete not reachable code

--- laff-pack.php
+++ laff-pack.php
@@ -49,21 +49,15 @@ class LAFFPack {
 				$this->container_dimensions = $this->_calc_container_dimensions();
 			}
 			else {
-				// Calculate container size
-				if(!is_array($container)) {
-					$this->container_dimensions = $this->_calc_container_dimensions();
+				if(!array_key_exists('length', $container) ||
+					!array_key_exists('width', $container)) {
+						throw new InvalidArgumentException("Function _pack only accepts array (length, width, height) as argument for $container");
 				}
-				else {
-					if(!array_key_exists('length', $container) ||
-						!array_key_exists('width', $container)) {
-							throw new InvalidArgumentException("Function _pack only accepts array (length, width, height) as argument for $container");
-					}
+			
+				$this->container_dimensions['length'] = $container['length'];
+				$this->container_dimensions['width'] = $container['width'];
 				
-					$this->container_dimensions['length'] = $container['length'];
-					$this->container_dimensions['width'] = $container['width'];
-					
-					// Note: do NOT set height, it will be calculated on-the-go
-				}
+				// Note: do NOT set height, it will be calculated on-the-go
 			}
 		}
 	}
